e219168be36947c7d21d741b777c62e36dc9304f
FujiwaraNaoto <naotofujiwara890@gmail.com>
2022-11-06 12:04:50 +0900

    add AOJ0005

---
+++ cin.cpp
@@ -0,0 +1,24 @@
+/*
+
+入力がなくなるまで読み込む
+*/
+
+#include<iostream>
+#include<set>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+
+
+
+int main(){
+    int a,b;
+    while(std::cin>>a>>b && !std::cin.eof()){
+        
+        std::cout<<a<<b<<std::endl;
+        
+    }
+}
