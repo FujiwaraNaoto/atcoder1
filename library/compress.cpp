77ecf17882ae2b918cc775ae4436c8739fcc23f0
FujiwaraNaoto <naotofujiwara890@gmail.com>
2023-02-26 11:17:23 +0900

    add some method to compression

--- library/compress.cpp
+++ library/compress.cpp
@@ -18,3 +18,31 @@ void comp(std::vector<int>& x){
         p=mp[p];
     }
 }
+
+
+//座標圧縮
+template <class T> struct compress{
+    
+    std::set<T> st;
+    std::map<T,int> mp;
+    int sz;
+    compress(const std::vector<T>& a):sz(0){
+        for(auto x:a){
+            st.insert(x);
+        }
+        
+        for(auto x:st){
+            mp[x]=sz++;
+        }
+    }
+
+    int val(const T& x){
+        //ベクトルaの要素xに対応した要素を取り出す
+        return mp[x];
+    }
+
+    int size(){
+        return sz;
+    }
+
+};
