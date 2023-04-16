a07a95d04635d7c78821dfe171a86d2f66a5c4d9
FujiwaraNaoto <naotofujiwara890@gmail.com>
2023-04-16 17:08:16 +0900

    modified mincost_flow

--- library/max_flow2.cpp
+++ library/max_flow2.cpp
@@ -1,219 +1,232 @@
 /*
 ABC225
 */
+
+//ABC247GはこれだとTLE
+//とりあえず保存
 #include<iostream>
+#include<set>
+#include<map>
 #include<vector>
+#include<algorithm>
 #include<queue>
-//using namespace std;
+#include<tuple>
+#include<cmath>
+#include<functional>
+#include<numeric>
+#include<string>
+using namespace std;
 typedef long long ll;
-const ll INF=1LL<<59;
+typedef std::pair<int,int> P;
+const ll INF=1e12;
 
-//ABC205 grid and tokens
-template<class T> //intになるかllになるか
-class FLOW{
+//using namespace std;
+/*
+mincost_flow<int, ll> graph;
+として用いる
+*/
+template<class Cap,class Cost>
+    class mincost_flow{
+
+    private:
+
+        struct edge{
+            int from;
+            int to;
+            Cap cap;
+            Cap flow;//どれだけ流したのか 一番最初は0
+            Cost cost;//どれだけのコストがかかるか
+            int rev;//逆辺
+
+            edge(int from_,int to_,Cap cap_,Cap flow_,Cost cost_,int rev_):from(from_),to(to_),cap(cap_),flow(flow_),cost(cost_),rev(rev_){};
+        };
+        //typedef std::pair<int,int> P;
+
+        std::set<pair<int,int>> fromto;//順辺のみ記録
+        //頂点数がV 
+        std::vector<Cost> h;
+        std::vector<Cost> dist;
+        std::vector<int> prevv;//prevv[v]はvの直前の頂点
+        std::vector<int> preve;//preve[v]はvの直前の逆辺のindex
+        std::vector<std::vector<edge>> G;
+
     public:
-    struct edge{int from; int to;  T cap; int rev;};    
-    std::vector<T> level;
-    std::vector<T> iter;
-   
-    std::vector<std::vector<edge> > G{};//vector
-
-    int numV=0;
-
-    //constructor
-    FLOW(int v_):numV(v_){
+        int V;//頂点数
         
-        G.resize(V+1);//G[0],G[1],...,G[V]を作る
-        level = vector<T>(numV+1,0);
-        iter  = vector<T>(numV+1,0);
-    }
-    
-    ~FLOW(){
-        std::vector<T>().swap(level);
-        std::vector<T>().swap(iter);
-        
-    }
 
-    void add_edge(int from,int to,T cap){
-        G[from].push_back((edge){from,to,cap,(int)G[to].size()} );
-        G[to].push_back((edge){to,from,0,(int)G[from].size()-1});
-    }
- 
- 
-    void bfs(int s){
-        //memset(level,-1,sizeof(level));
-        level = vector<T>(numV+1,-1);//-1に初期化
 
-        queue<int> que;
-        level[s]=0;
-        que.push(s);
-        while(!que.empty()){
-    
-            int p = que.front();que.pop();
-            for(int i=0;i<(int)G[p].size();i++){
-                edge e = G[p][i];//頂点pに繋がっている頂点が e.to
-                if(e.cap>0 && level[e.to]<0){
-                    level[e.to] = level[p]+1;
-                    que.push(e.to);
+
+        //constructor
+        mincost_flow(int v):V(v){
+            G = std::vector<std::vector<edge>>(V,std::vector<edge>{});
+            prevv = std::vector<int>(V,-1);
+            preve = std::vector<int>(V,-1);
+            h=std::vector<Cost>(V,0);//initialize h //potential h
+            dist = std::vector<Cost>(V,INF);
+        }
+
+
+        void add_edge(int from,int to,Cap cap,Cost cost){
+            //from, to, cap,flow,cost,rev
+            G[from].emplace_back(from,   to,cap, 0,     cost,(int)G[to].size() );
+            //逆辺の場合は一度流してしまうと考えてflowを考慮する
+            G[to].emplace_back( to, from,  0, cap,-cost,(int)G[from].size()-1);     
+           
+            fromto.insert(std::make_pair(from,to));//順辺を記録しておく
+
+        }
+
+        std::vector<edge> edges(){
+            std::vector<edge> ret;//vector instance edges
+             //辺を入れていく.
+            //ただし逆辺は入れない 順辺のみを入れる.
+            for(int i=0;i<V;i++){
+                for(edge &e:G[i]){
+                    if(fromto.find(std::make_pair(e.from,e.to)) != fromto.end() ){
+                        ret.push_back(e);
+                    }
                 }
             }
+
+            return ret;
         }
-    }
- 
-    T dfs(int v,int t,T f){
-        if(v==t) return f;
-        for(T &i=iter[v]; i<(T)G[v].size(); i++){
-            edge &e = G[v][i];
-            if(e.cap>0 && level[v]<level[e.to]){
-                T d = dfs(e.to, t, min(f,e.cap));
-                if(d>0){
-                    e.cap -= d;
-                    G[e.to][e.rev].cap += d;
-                    return d;
+
+
+    //流れた量fと最小コストres
+    std::pair<Cap,Cost> flow(int start,int goal,Cap f){
+        
+        //f流すことを目的とする
+        Cap maxflow=f;
+       
+        Cost mincost=0;
+
+        //h=std::vector<Cost>(V,0);
+        std::fill(h.begin(), h.end(), 0);
+
+        while(f>0){
+
+            std::priority_queue<std::pair<Cost,int>,vector<std::pair<Cost,int>>, greater<std::pair<Cost,int>>> que; 
+
+            //dist = std::vector<Cost>(V,INF);
+            std::fill(dist.begin(), dist.end(), INF);//これで高速になるみたい
+
+            dist[start]=0;
+            que.push(std::make_pair(0,start));//頂点番号start 最短距離0
+
+            while(!que.empty()){                
+                //ここが遅いかな
+                auto p= que.top(); que.pop();
+                int v = p.second;
+                
+
+                if(dist[v]<p.first) continue;
+                
+                
+
+                for(int i=0;i<(int)G[v].size();i++){
+                    edge &e = G[v][i];
+                    if(e.cap>0 && dist[e.to] > dist[v]+e.cost+h[v]-h[e.to]){
+                        dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
+                        prevv[e.to]=v;
+                        preve[e.to]=i;
+                        que.push( std::make_pair(dist[e.to],e.to) );   
+                    }
                 }
             }
-        }
- 
-        return 0;
-    }
- 
- 
-    T max_flow(int s, int t){
-        T flow=0;
-        while(1){
-            bfs(s);
-            if(level[t]<0) return flow;
-            //memset(iter,0,sizeof(iter));
-            iter = vector<T>(numV+1,0);//0に初期化
 
-            T f;
-            while((f=dfs(s,t,INF))>0 ){
-                flow += f;
+            
+            que= decltype(que)();
+
+            if(dist[goal]==INF){
+                //return -1;
+                //maxflowすべてを流すことが無理
+                //cout<<"no"<<endl;
+                
+                //return make_pair(-1,mincost);
+                
+                
+                
+                //とりあえず,流すことができた量を答える
+                return make_pair(maxflow-f,mincost);
+
+            }
+
+            for(int v=0;v<V;v++){
+                h[v]+=dist[v];
+            }
+
+            Cap d = f;
+            //goalから逆にたどっていく
+            for(int v=goal;v!=start;v = prevv[v]){
+                d = min(d,G[prevv[v]][preve[v]].cap);
+            }
+
+            f-=d;
+            mincost += d*h[goal];
+            for(int v=goal;v!=start;v=prevv[v]){
+                edge &e = G[prevv[v]][preve[v]];//prevv[v]->preve[v]に流量dを流す.
+                e.cap -= d;//辺eにdを追加で流す
+                e.flow +=d;//辺eにdを追加で流す
+                G[v][e.rev].cap += d;
+                G[v][e.rev].flow -=d;
             }
         }
+
+        
+        //流れた量(maxflow-f)と最小コストminres
+        
+        return make_pair(maxflow-f,mincost);
     }
 
 
-    std::vector<edge> edges(){
-        std::vector<edge> ret;
-        for(int i=0;i<=numV;i++){
-            for(auto e: G[i]){//二部グラフの場合cap==0の時にその経路を通ったということ
-                ret.push_back(e);
-            }
-        }
-        return ret;
-    }
-    
 };
 
-
 int main(){
-    int H,W,C;
-    cin >> H >> W >> C;
-    FLOW<ll> graph(H*W+2);
+    int N;
+    cin>>N;
 
-    vector<vector<int> > A(H,vector<int>(W));
-    /*
-    rep(i,0,H){
-        rep(j,0,W) cin >> A[i][j];
+    mincost_flow<int,ll> graph(150+150+10);
+
+    //1~150が大学
+    // 151~300が得意分野
+    vector<int> A(N),B(N);
+    vector<ll> C(N);
+    for(int i=0;i<N;i++){
+        cin>>A[i]>>B[i]>>C[i];
+        //void add_edge(int from,int to,Type cap,Type cost){
+        graph.add_edge(A[i],150+B[i],1,INF-C[i]);
     }
-    */
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            cin>>A[i][j];
-        }
+    int source=0, sink= 301;
+    //
+    for(int univ=1;univ<=150;univ++){
+        graph.add_edge(source,univ,1,0);
     }
-    
-    
-    
-    int sz = H*W;
-    /*
-    rep(i,0,H){
-        rep(j,0,W){
-            int now = i*W+j;
-            graph.add_edge(sz,now,A[i][j]);
-        }
-    }
-    */
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            int now = i*W+j;
-            graph.add_edge(sz,now,A[i][j]);
-        }
-    }
-    /*
-    rep(i,0,H){
-        rep(j,0,W){
-            int now = i*W+j;
-            if(0 < i && 0 < j){
-                int next = (i-1)*W+(j-1);
-                graph.add_edge(now,sz+1,0);
-                graph.add_edge(now,next,C);
-            }
-            else{
-                graph.add_edge(now,sz+1,C);
-            }
-        }
-    }
-    */
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            int now = i*W+j;
-            if(0 < i && 0 < j){
-                int next = (i-1)*W+(j-1);
-                graph.add_edge(now,sz+1,0);
-                graph.add_edge(now,next,C);
-            }
-            else{
-                graph.add_edge(now,sz+1,C);
-            }
-        }
+
+    for(int sp=1;sp<=150;sp++){
+        graph.add_edge(150+sp,sink,1,0);
     }
 
 
-    /*
-    rep(i,0,H){
-        rep(j,0,W){
-            int now = i*W+j;
-            if(0 < i && j+1 < W){
-                int next = (i-1)*W+(j+1);
-                graph.add_edge(now,sz+1,0);
-                graph.add_edge(now,next,C);
-            }
-            else{
-                graph.add_edge(now,sz+1,C);
-            }
+    vector<ll> ans;
+
+    for(int k=1;k<=150;k++){
+        //流量kを流すことを考える
+
+      
+        mincost_flow tmpg=graph;
+        auto [maxflow,mincost]=tmpg.flow(source,sink,k);
+        
+        if(maxflow<k){
+           
+            break;
         }
-    }
-    */
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            int now = i*W+j;
-            if(0 < i && j+1 < W){
-                int next = (i-1)*W+(j+1);
-                graph.add_edge(now,sz+1,0);
-                graph.add_edge(now,next,C);
-            }
-            else{
-                graph.add_edge(now,sz+1,C);
-            }
-        }
+      
+        ans.push_back(INF*k-mincost);
     }
 
-
-    ll ans = 0;
-    /*
-    rep(i,0,H){
-        rep(j,0,W) ans += A[i][j];
-    }
-    */
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            ans += A[i][j];
-        }
+    cout<<ans.size()<<endl;
+    for(ll v:ans){
+        cout<<v<<endl;
     }
 
-
-    ans -= graph.max_flow(sz,sz+1);
-    cout << ans << endl;
 }
