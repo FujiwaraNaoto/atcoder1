4d35c683b9668839f74037ed296a855126c127cb
FujiwaraNaoto <naotofujiwara890@gmail.com>
2023-06-07 19:31:51 +0900

    add some chineserem function

--- library/ChineseRem.cpp
+++ library/ChineseRem.cpp
@@ -36,3 +36,83 @@ std::pair<long long, long long> ChineseRem(const std::vector<long long> &b, cons
   }
   return std::make_pair(mod(r, M), M);
 }
+
+
+
+
+
+
+/*
+
+x ≡ b1 (mod m1)
+x ≡ b2 (mod m2)
+を解く
+
+return first= xの解
+return second= mod
+
+*/
+std::pair<long long,long long> chinese_rem(long long b1,long long m1,long long b2, long long m2){
+    
+    //d=m1*p+m2*q
+    auto [d,p,q] = ext_gcd(m1,m2);
+
+
+    long long lcm=(m1/d)*m2;
+    
+    if((b2-b1)%d !=0){
+        //(r,m)  = r (mod m)
+        return std::make_pair(0,-1);
+    }
+
+    long long c=(b2-b1)/d;
+   
+   
+    c*=p;
+
+    c%=(m2/d);
+    c*=m1;
+    c%=lcm;
+
+    long long x=b1+c;
+    x%=lcm;
+    x+=lcm;
+    x%=lcm;
+
+    return std::make_pair(x,lcm);
+};
+
+
+
+long long gcd(long long x,long long y){
+    return y==0?x:gcd(y,x%y);
+}
+
+long long lcm(long long x,long long y){
+
+    return x/gcd(x,y)*y;
+}
+
+
+std::pair<long long,long long> chinese_rem2(const std::vector<long long>& b,const std::vector<long long>& m){
+
+    std::assert(b.size()==m.size());
+    int n=b.size();
+
+    long long x=0;
+    long long M=1;
+    for(int i=0;i<n;i++){
+        
+        auto [c,flag]=chinese_rem(x,M,b[i],m[i]);
+        if(flag==-1){
+            return make_pair(0,-1);
+        }
+
+        M=lcm(M,m[i]);
+        x=c;
+        
+    }
+    
+    return make_pair((x%M + M)%M ,M);
+}
+
