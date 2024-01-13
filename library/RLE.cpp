7324f89994de395dc9199522f2b6f6f7a2c8ca46
FujiwaraNaoto <naotofujiwara890@gmail.com>
2024-01-13 18:33:54 +0900

    add RLE

---
+++ library/RLE.cpp
@@ -0,0 +1,31 @@
+/*
+ランレングス圧縮
+*/
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <queue>
+#include <random>
+#include <string>
+#include <vector>
+#include<fstream>
+typedef long long ll;
+
+std::vector<std::pair<char,int>> compress(const string& s){
+    int n=s.size();
+    std::vector<std::pair<char,int>> res;
+    int cnt=1;
+    for(int i=0;i<n;i++){
+        if(i+1<n && s[i]==s[i+1]){
+            cnt++;//i+1のぶん
+        }else{
+            //i=n-1 or s[i]!=s[i+1]
+            if(cnt>0){
+                res.emplace_back(s[i],cnt);
+            }
+            cnt=1;//i+1のぶん
+        }
+    }
+    return res;
+}
